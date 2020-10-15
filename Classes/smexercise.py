314a7b56448940dbb0a8d8287d7bc6eca6573945
davidvaughan86 <david.vaughan86@gmail.com>
2020-10-15 17:37:36 -0400

    first import

---
+++ Classes/smexercise.py
@@ -0,0 +1,31 @@
+# user_name = input("what is your name?\n")
+# greeting = ("hello %s its nice to meeet you." % (user_name))
+# print(greeting)
+
+# greeting = ("hey %s your name has five letters in it too" % (user_name))
+# print(greeting.upper())
+
+# name = input("what is your name?\n")
+# subject = input("what is your favorite subject\n")
+# print("%s 's favorite subject in school is %s" % (name, subject))
+
+day = int(input( 'enter a day (0-6)?'))
+elif day == 0:
+print ("sunday")
+elif day == 1
+print ("monday")
+elif day == 2
+print ("tuesday")
+elif day == 3
+print ("wednesday")
+elif day == 4
+print ("thursday")
+elif day == 5
+print ("friday")
+elif day == 6
+print ("saturday")
+
+# day_num = int(input(" how many days into the week are wek? enter 0-6\n"))
+# days = ("sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday")
+# print(day[day_num])
+
