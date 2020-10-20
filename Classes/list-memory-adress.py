29c9f796127071ca0d4ea11255b2ad4f563db674
davidvaughan86 <david.vaughan86@gmail.com>
2020-10-20 16:51:29 -0400

    origin

---
+++ Classes/list-memory-adress.py
@@ -0,0 +1,32 @@
+# print(id("a"))
+
+# x = "a"
+# print(id(x))
+
+# x= 1
+# print(id(x))
+
+# print(id("a"))
+
+
+a = [1,2,3]
+b = [1,2,3]
+
+print(a)#similar in value but not the same memory adress
+print(b)# using a = b would give the same address
+
+c = [1, 2, 3, 4]
+d = c
+print(id(c) , id(d))
+
+c.append(4)
+print(c,d) #uses this to append memory address by reassiging it to a new value that is equal to the same memory address
+
+
+
+people = [
+    ["david", "vaughan" , 34],
+    ["diana", "vaughan" , 37]
+]
+
+print(people[0][2])
